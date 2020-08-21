252ab8daf69bc82a2e3f19d70b96c4641e8532d9
Karim Bellioum <kbellioum@gmail.com>
2020-08-21 16:32:12 +0100

    Initial commit

---
+++ App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1>TEST MapBox</h1>
+    <div id="map"></div>
+  </div>
+</template>
+
+<script>
+import mapboxgl from "mapbox-gl";
+
+export default {
+  name: "App",
+  mounted() {
+    mapboxgl.accessToken =
+      "pk.eyJ1Ijoia2JlbGxpb3VtIiwiYSI6ImNrZTRhcmF4cDAzaGszMW8wNDV2Z2QzZzcifQ.J5LPD_0NDU974meLRh5qBw";
+    new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-74.5, 40],
+      zoom: 6
+    });
+  }
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#map {
+  height: 100vh;
+}
+</style>
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+import mapboxgl from "mapbox-gl";
+
+export default {
+  name: "App",
+  mounted() {
+    mapboxgl.accessToken =
+      "pk.eyJ1Ijoia2JlbGxpb3VtIiwiYSI6ImNrZTRhcmF4cDAzaGszMW8wNDV2Z2QzZzcifQ.J5LPD_0NDU974meLRh5qBw";
+    new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-74.5, 40],
+      zoom: 6
+    });
+  }
+};
+</script>
+
+<style scoped>
+#map {
+  height: 100vh;
+}
+</style>
